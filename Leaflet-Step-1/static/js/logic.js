2296bcfc4971520bf758ea0c8739b1c36a3178d3
pkorat <pkorat@github.com>
2021-09-02 22:39:55 -0500

    switched to all_day json earthquakes and added in markers for earthquake location

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,3 +7,19 @@ var myMap = L.map("map", {
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
+
+var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+
+d3.json(url).then(function(response) {
+
+    console.log(response.features)
+
+    for (var i=0; i < response.features.length; i++) {
+
+        var location = response.features[i].geometry
+
+        if (location) {
+            L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap)
+        }
+    }
+})
